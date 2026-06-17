f7d76e73e5991e441bcc245653f62b42544d80aa
suzuki-25 <pyon.la2i.s@gmail.com>
2026-06-17 14:27:51 +0900

    PostHogをインストールし設定。

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// ① PostHogを読み込む
+import posthog from 'posthog-js';
+
+// ② PostHogを初期化する（このブロックを追記）
+// ※APIキーとHost URLをセット
+posthog.init('phc_kJwQgtBGa5rxYEvph4EWDst8JzoKsgtWvMsXqm9B5BCp', {
+  api_host: 'https://us.i.posthog.com',
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
